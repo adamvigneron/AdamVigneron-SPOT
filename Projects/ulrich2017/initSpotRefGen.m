6be9271ed6edbe2fc0745191f979d15a0b1707e7
Adam Vigneron <adamvigneron@cmail.carleton.ca>
2024-10-25 14:41:06 -0400

    Completed removal of reference spinup functionality

--- Projects/ulrich2017/initSpotRefGen.m
+++ Projects/ulrich2017/initSpotRefGen.m
@@ -1,30 +1,3 @@
-%% configuration switches
-
-includeSpinup = 0;
-
-
-%% additional calculations
-
-% as an initial conservative design, reserve 90% of the authority 
-% and split the remaining 10% between spin-up and centripetal
-
-% determine translational authority allocated for spin-up and centripetal
-alloc    = 0.1;
-traAuth  = min(F_red_X_nominal,F_red_Y_nominal) / mRED;
-accAvail = alloc * traAuth;
-
-% reference orbit 
-rRef   = 0.85;  % radius, metres
-omgRef = 0.03490659;  % angular frequency, rad/s
-
-% calculate the centripetal and spin-up accelerations
-accCent = omgRef^2 * rRef;
-accSpin = accAvail - accCent;
-
-% finally, calculate the total spin-up time
-spinupTime = omgRef * rRef / accSpin;
-
-
 %% predeclare for code generation
 numPhase = length(meta.class.fromName('SpotPhase').EnumerationMemberList);
 numCoord = length(meta.class.fromName('SpotCoord').EnumerationMemberList);
@@ -38,17 +11,19 @@ structRefGen.k5  = 0;
 
 paramRefGen = repmat(structRefGen,numPhase,numCoord);
 
+
+%% reference orbit
+
+rRef   = 0.85;  % radius, metres
+omgRef = 0.03490659;  % angular frequency, rad/s
+
+
 %% SpotPhase.Phase3_4 - SpotCoord.xRed
 
 phase = SpotPhase.Phase3_4;
 coord = SpotCoord.xRed;
 
-if includeSpinup
-    paramRefGen(phase,coord).fun = SpotGnc.refCosineSpinup;
-    paramRefGen(phase,coord).k5  = spinupTime;
-else
-    paramRefGen(phase,coord).fun = SpotGnc.refCosine;
-end
+paramRefGen(phase,coord).fun = SpotGnc.refCosine;
 
 % ref = k1 * cos( k2 * t + k3 ) + k4
 paramRefGen(phase,coord).k1  = rRef;
@@ -62,12 +37,7 @@ paramRefGen(phase,coord).k4  = init_states_RED(1) - rRef;
 phase = SpotPhase.Phase3_4;
 coord = SpotCoord.yRed;
 
-if includeSpinup
-    paramRefGen(phase,coord).fun = SpotGnc.refSineSpinup;
-    paramRefGen(phase,coord).k5  = spinupTime;
-else
-    paramRefGen(phase,coord).fun = SpotGnc.refSine;
-end
+paramRefGen(phase,coord).fun = SpotGnc.refSine;
 
 % ref = k1 * sin( k2 * t + k3) + k4;
 paramRefGen(phase,coord).k1  = rRef;
